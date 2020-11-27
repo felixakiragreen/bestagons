0da982b36d9fa031833e788bb7dd11d79cb0190c
Felix Green <work@felix.green>
2020-11-27 16:05:06 -0500

    add Xcode project

---
+++ Bestagons/Shared/BestagonsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BestagonsApp.swift
+//  Shared
+//
+//  Created by Felix Akira Green on 11/27/20.
+//
+
+import SwiftUI
+
+@main
+struct BestagonsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
