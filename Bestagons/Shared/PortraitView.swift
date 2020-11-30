965368c670604941f6a64b4165aa07cb056c414f
Felix Green <work@felix.green>
2020-11-29 21:41:24 -0800

    stub new portrait view

---
+++ Bestagons/Shared/PortraitView.swift
@@ -0,0 +1,20 @@
+//
+//  PortraitView.swift
+//  Bestagons
+//
+//  Created by Felix Akira Green on 11/29/20.
+//
+
+import SwiftUI
+
+struct PortraitView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct PortraitView_Previews: PreviewProvider {
+    static var previews: some View {
+        PortraitView()
+    }
+}
