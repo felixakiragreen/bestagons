b18e5767e2bf6fa1c39c3ee7ca88450581438d83
Felix Green <work@felix.green>
2020-12-02 23:50:19 -0500

    add inverseMask extension to View

--- Bestagons/Shared/BestagonsApp.swift
+++ Bestagons/Shared/BestagonsApp.swift
@@ -15,3 +15,20 @@ struct BestagonsApp: App {
         }
     }
 }
+
+// Copied from https://www.raywenderlich.com/7589178-how-to-create-a-neumorphic-design-with-swiftui#toc-anchor-012
+extension View {
+	// Define a new inverseMask that mimics mask
+	func inverseMask<Mask>(_ mask: Mask) -> some View where Mask: View {
+		// Return the current view masked with the input mask and modified.
+		self.mask(mask
+			// Set the foreground color of the input mask to black.
+			.foregroundColor(.black)
+			// Ensure the background of the input mask is solid white.
+			.background(Color.white)
+			// Wrap the input mask in a compositing group.
+			.compositingGroup()
+			// Converted the luminance to alpha, turning the black foreground transparent and keeping the light background opaque — i.e., an inverse mask!
+			.luminanceToAlpha())
+	}
+}
