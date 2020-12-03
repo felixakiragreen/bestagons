14b62076d0e8f200c5ca7e7b9cff33212d78679b
Felix Green <work@felix.green>
2020-12-02 23:51:23 -0500

    add shadows to the portrait

--- Bestagons/Shared/PortraitView.swift
+++ Bestagons/Shared/PortraitView.swift
@@ -11,26 +11,30 @@ struct PortraitView: View {
 	// MARK: - PROPERTIES
 
 	@State var animationTimeIsForward = true
-	@State var animationDuration = 3.0
+	@State var animationDuration = 2.0
 
 	let initialVisible = true
-	
+
 	@State var bgVisible = true
 	var bgDelayFraction = 0.0
-	var bgDurationFraction = 1.0
+	var bgDurationFraction = 0.9
 
 	@State var headVisible = true
-	var headDelayFraction = 0.5
-	var headDurationFraction = 1.0
+	var headDelayFraction = 0.3
+	var headDurationFraction = 1.2
 
 	@State var chestVisible = true
-	var chestDelayFraction = 1.0
-	var chestDurationFraction = 1.0
+	var chestDelayFraction = 0.6
+	var chestDurationFraction = 1.2
 
 	@State var hairVisible = true
-	var hairDelayFraction = 1.5
-	var hairDurationFraction = 1.0
+	var hairDelayFraction = 0.9
+	var hairDurationFraction = 1.2
 
+	@State var shadowVisible = true
+	var shadowDelayFraction = 0.9
+	var shadowDurationFraction = 1.8
+	
 	// MARK: - BODY
 
 	var body: some View {
@@ -38,12 +42,17 @@ struct PortraitView: View {
 			bgDurationFraction,
 			headDurationFraction,
 			chestDurationFraction,
-			hairDurationFraction
+			hairDurationFraction,
 		].reduce(0, +)
 
 		return ZStack {
-			Color.black.edgesIgnoringSafeArea(.all)
-
+			Color("grey.500").edgesIgnoringSafeArea(.all)
+//				.onAppear({
+//					bgVisible = false
+//					headVisible = false
+//					chestVisible = false
+//					hairVisible = false
+//				})
 
 			HStack {
 				VStack(spacing: 16) {
@@ -57,11 +66,11 @@ struct PortraitView: View {
 							Text("hairVisible → \(String(hairVisible))")
 						}
 						Spacer()
-					}//: HSTACK LABELS
+					} //: HSTACK LABELS
 
 					// MARK: - ANIMATION SPECS
 
-					Button("toggle") {
+					Button("animated") {
 						// flip direction
 						animationTimeIsForward.toggle()
 
@@ -100,6 +109,24 @@ struct PortraitView: View {
 						) {
 							hairVisible.toggle()
 						}
+						
+						// SHADOW
+						withAnimation(
+							Animation
+								.easeInOut(duration: shadowDurationFraction / fractionalDuration * animationDuration)
+								.delay(shadowDelayFraction / fractionalDuration * animationDuration)
+						) {
+							shadowVisible.toggle()
+						}
+					}
+					
+					Button("instant") {
+						// flip direction
+						bgVisible.toggle()
+						headVisible.toggle()
+						chestVisible.toggle()
+						hairVisible.toggle()
+						shadowVisible.toggle()
 					}
 
 					VStack(alignment: .leading) {
@@ -113,18 +140,20 @@ struct PortraitView: View {
 					ZStack {
 						Background(visible: bgVisible)
 
-						Chest(size: geometry.size, visible: chestVisible)
+						Chest(size: geometry.size, show: chestVisible, showShadow: shadowVisible)
 
-						Bun(size: geometry.size, visible: hairVisible)
+						Bun(size: geometry.size, show: hairVisible, showShadow: shadowVisible)
 
-						Head(size: geometry.size, visible: headVisible)
+						Head(size: geometry.size, show: headVisible, showShadow: shadowVisible)
 
-						Hairline(size: geometry.size, visible: hairVisible)
+						Hairline(size: geometry.size, show: hairVisible, showShadow: shadowVisible)
 					} //: ZSTACK
 					.frame(maxWidth: .infinity, maxHeight: .infinity)
+//					.drawingGroup()
 				} //: RIGHT
 				.aspectRatio(1, contentMode: .fit)
 				.padding(40)
+				.background(Color.black)
 			}
 		}
 	}
@@ -156,92 +185,162 @@ struct Background: View {
 
 struct Head: View {
 	let size: CGSize
-	let visible: Bool
+	let show: Bool
+	let showShadow: Bool
 
 	var body: some View {
-		PolygonShape(sides: 6)
-			.frame(
-				maxWidth: visible ? size.width * 12 / 24 : 0,
-				maxHeight: visible ? size.height * 12 / 24 : 0,
-				alignment: .center
-			)
-			.foregroundColor(Color("grey.100"))
+		ZStack {
+			Color("grey.100")
+			LinearGradient(
+				gradient: Gradient(colors: [
+					Color.white,
+					Color.black,
+				]),
+				startPoint: UnitPoint(x: showShadow ? 0.49999 : 0.0, y: 0.0),
+				endPoint: UnitPoint(x: showShadow ? 0.50001 : 1.0, y: 0.0)
+			) //: LINEAR GRADIENT
+			.opacity(0.12)
+			.blendMode(.darken)
+		} //: FACE
+		.frame(
+			maxWidth: size.width * 12 / 24,
+			maxHeight: size.height * 12 / 24,
+			alignment: .center
+		)
+		.mask(
+			PolygonShape(sides: 6)
+				.scale(show ? 1 : 0)
+		)
 	}
 }
 
 struct Chest: View {
 	let size: CGSize
-	let visible: Bool
+	let show: Bool
+	let showShadow: Bool
 
 	var body: some View {
-		Group {
-			// SHIRT
-			PolygonShape(sides: 6)
-				.frame(
-					maxWidth: size.width,
-					maxHeight: size.height,
-					alignment: .center
-				)
-				.foregroundColor(Color("grey.900"))
-				.offset(y: size.height * 0.6)
-				.clipShape(PolygonShape(sides: 6))
+		ZStack {
+			Group {
+				Color("grey.900")
+				LinearGradient(
+					gradient: Gradient(colors: [
+						Color.white,
+						Color.black,
+					]),
+					startPoint: UnitPoint(x: showShadow ? 0.49999 : 0.0, y: 0.0),
+					endPoint: UnitPoint(x: showShadow ? 0.50001 : 1.0, y: 0.0)
+				) //: LINEAR GRADIENT
+				.opacity(0.12)
+				.blendMode(.lighten)
+			}//: SHIRT
 
-			// BODY
+			Group {
+				Color("grey.200")
+				LinearGradient(
+					gradient: Gradient(colors: [
+						Color.white,
+						Color.black,
+					]),
+					startPoint: UnitPoint(x: showShadow ? 0.49999 : 0.0, y: 0.0),
+					endPoint: UnitPoint(x: showShadow ? 0.50001 : 1.0, y: 0.0)
+				) //: LINEAR GRADIENT
+				.opacity(0.12)
+				.blendMode(.darken)
+			}//: BODY
+			.mask(
+				PolygonShape(sides: 6)
+					.frame(
+						maxWidth: size.width * 16 / 24,
+						maxHeight: size.height * 16 / 24,
+						alignment: .center
+					)
+					.foregroundColor(.black)
+			)
+		} //: ZSTACK → SHIRT & BODY
+		.mask(
 			PolygonShape(sides: 6)
-				.frame(
-					maxWidth: size.width,
-					maxHeight: size.height,
-					alignment: .center
-				)
-				.foregroundColor(Color("grey.200"))
-				.offset(y: -(size.height * 4 / 24))
-				.mask(
-					PolygonShape(sides: 6)
-						.offset(y: size.height * 0.6)
-				)
-		} //: SHIRT & BODY
-		.offset(y: visible ? 0 : size.height * 4 / 24)
-		.opacity(visible ? 1 : 0)
+				.offset(y: size.height * 15 / 24)
+		)
+		.mask(
+			PolygonShape(sides: 6)
+				.scale(show ? 1 : 0)
+		)
+		.offset(y: show ? 0 : -(size.height * 3 / 24))
+		.opacity(show ? 1 : 0)
 	}
 }
 
 struct Hairline: View {
 	let size: CGSize
-	let visible: Bool
+	let show: Bool
+	let showShadow: Bool
 
 	var body: some View {
-		PolygonShape(sides: 6)
-			.frame(
-				maxWidth: visible ? size.width * 6 / 24 : 0,
-				maxHeight: visible ? size.height * 6 / 24 : 0,
-				alignment: .center
-			)
-			.foregroundColor(Color("yellow.700"))
-			.offset(y: -size.height * 6 / 24)
-			.opacity(visible ? 1 : 0)
-			.mask(
-				PolygonShape(sides: 6)
-					.frame(
-						width: size.width * 12 / 24,
-						height: size.height * 12 / 24
-					)
-			)
+		ZStack {
+			Color("yellow.700")
+			LinearGradient(
+				gradient: Gradient(colors: [
+					Color.white,
+					Color.black,
+				]),
+				startPoint: UnitPoint(x: showShadow ? 0.49999 : 0.0, y: 0.0),
+				endPoint: UnitPoint(x: showShadow ? 0.50001 : 1.0, y: 0.0)
+			) //: LINEAR GRADIENT
+			.opacity(0.12)
+			.blendMode(.darken)
+		}//: ZSTACK
+		.frame(
+			maxWidth: size.width * 12 / 24,
+			maxHeight: size.height * 12 / 24,
+			alignment: .center
+		)
+		.mask(
+			StretchyHexagon(trunk: 0)
+				.frame(
+					width: size.width * 6 / 24,
+					height: size.height * 6 / 24
+				)
+				.scaleEffect(show ? 1 : 0)
+		)
+		.offset(y: show ? -(size.height * 3 / 24) : -(size.height * 6 / 24))
+		.opacity(show ? 1 : 0)
 	}
 }
 
 struct Bun: View {
 	let size: CGSize
-	let visible: Bool
+	let show: Bool
+	let showShadow: Bool
 
 	var body: some View {
-		PolygonShape(sides: 6)
-			.frame(
-				maxWidth: visible ? size.width * 4 / 24 : 0,
-				maxHeight: visible ? size.height * 4 / 24 : 0,
-				alignment: .center
-			)
-			.foregroundColor(Color("yellow.800"))
-			.offset(y: visible ? -size.height * 7 / 24 : -size.height * 6 / 24)
-			.opacity(visible ? 1 : 0)
+		ZStack {
+			Color("yellow.800")
+			LinearGradient(
+				gradient: Gradient(colors: [
+					Color.white,
+					Color.black,
+				]),
+				startPoint: UnitPoint(x: showShadow ? 0.49999 : 0.0, y: 0.0),
+				endPoint: UnitPoint(x: showShadow ? 0.50001 : 1.0, y: 0.0)
+			) //: LINEAR GRADIENT
+			.opacity(0.12)
+			.blendMode(.darken)
+		}//: ZSTACK
+		.frame(
+			maxWidth: show ? size.width * 4 / 24 : 0,
+			maxHeight: show ? size.height * 4 / 24 : 0,
+			alignment: .center
+		)
+		.mask(
+			PolygonShape(sides: 6)
+				.frame(
+					maxWidth: show ? size.width * 4 / 24 : 0,
+					maxHeight: show ? size.height * 4 / 24 : 0,
+					alignment: .center
+				)
+		)
+		.offset(y: show ? -(size.height * 7 / 24) : -(size.height * 6 / 24))
+		.opacity(show ? 1 : 0)
 	}
 }
