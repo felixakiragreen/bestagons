2ba28bc50c9c7f60c6e28b946aa7166aa37b6822
Felix Green <work@felix.green>
2020-12-14 22:13:13 -0500

    add Apparatus project

---
+++ Apparatus/Shared/ApparatusApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApparatusApp.swift
+//  Shared
+//
+//  Created by Felix Akira Green on 12/14/20.
+//
+
+import SwiftUI
+
+@main
+struct ApparatusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
