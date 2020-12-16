2cb59384d733142f70256e9ce991e4c5df99ff68
Felix Green <work@felix.green>
2020-12-16 13:10:02 -0500

    stub options

---
+++ Apparatus/Shared/ApparatusOptions.swift
@@ -0,0 +1,20 @@
+//
+//  ApparatusOptions.swift
+//  Apparatus
+//
+//  Created by Felix Akira Green on 12/16/20.
+//
+
+import SwiftUI
+
+struct ApparatusOptions: View {
+	var body: some View {
+		Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+	}
+}
+
+struct ApparatusOptions_Previews: PreviewProvider {
+	static var previews: some View {
+		ApparatusOptions()
+	}
+}
