6077c7f8a3ced6906b7605431b28e03a3ceab8a5
Felix Green <work@felix.green>
2020-12-18 12:52:15 -0500

    move generated art into ArtView

--- Apparatus/Shared/ArtView.swift
+++ Apparatus/Shared/ArtView.swift
@@ -8,13 +8,46 @@
 import SwiftUI
 
 struct ArtView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+	@Binding var config: ApparatusConfig
+	@Binding var options: ApparatusOptions
+	
+	var rects: [BlockRect]
+	var size: CGFloat = 8
+	var pad: CGFloat = 0
+
+	var body: some View {
+		ZStack(alignment: .topLeading) {
+			ForEach(rects, id: \.id) { rect in
+				let w = CGFloat(rect.w) * (size + pad) - pad
+				let h = CGFloat(rect.h) * (size + pad) - pad
+				let x = CGFloat(rect.x1) * (size + pad)
+				let y = CGFloat(rect.y1) * (size + pad)
+				
+				RoundedRectangle(cornerRadius: CGFloat(options.rounding), style: .continuous)
+					.foregroundColor(rect.color.opacity(options.showFill ? 1 : 0))
+					.frame(width: w, height: h)
+					.border(Color("grey.900").opacity(0.5), width: options.showStroke ? 1 : 0)
+					.overlay(options.showDebug ? Text("\(rect.id)")
+						.font(.caption)
+						: nil
+					)
+					.offset(x: x, y: y)
+			}
+		}//: ZSTACK - Apparatus
+	}
 }
 
 struct ArtView_Previews: PreviewProvider {
-    static var previews: some View {
-        ArtView()
-    }
+	static let config = ApparatusConfig()
+	static let apparatus = ApparatusGenerator(config: config)
+	static let grid = apparatus.generate()
+	static let rects = convertLineGridToRect(grid: grid)
+	
+	static var previews: some View {
+		ArtView(
+			config: .constant(config),
+			options: .constant(ApparatusOptions()),
+			rects: rects
+		)
+	}
 }
--- Apparatus/Shared/ContentView.swift
+++ Apparatus/Shared/ContentView.swift
@@ -49,24 +49,7 @@ struct ContentView: View {
 					VStack {
 						Spacer()
 						
-						ZStack(alignment: .topLeading) {
-							ForEach(rects, id: \.id) { rect in
-								let w = CGFloat(rect.w) * (size + pad) - pad
-								let h = CGFloat(rect.h) * (size + pad) - pad
-								let x = CGFloat(rect.x1) * (size + pad)
-								let y = CGFloat(rect.y1) * (size + pad)
-								
-								RoundedRectangle(cornerRadius: CGFloat(options.rounding), style: .continuous)
-									.foregroundColor(rect.color.opacity(options.showFill ? 1 : 0))
-									.frame(width: w, height: h)
-									.border(Color("grey.900").opacity(0.5), width: options.showStroke ? 1 : 0)
-									.overlay(options.showDebug ? Text("\(rect.id)")
-										.font(.caption)
-										: nil
-									)
-									.offset(x: x, y: y)
-							}
-						}//: ZSTACK - Apparatus
+						ArtView(config: $config, options: $options, rects: rects, size: size, pad: pad)
 						.frame(
 							width: CGFloat(apparatus.xDim) * (size + pad),
 							height: CGFloat(apparatus.yDim) * (size + pad),
