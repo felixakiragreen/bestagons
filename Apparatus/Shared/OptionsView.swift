e888cc77ddd5cb5f8db346c926f9e9db4160d74f
Felix Green <work@felix.green>
2020-12-18 22:56:12 -0500

    add a popover for picking colors

--- Apparatus/Shared/OptionsView.swift
+++ Apparatus/Shared/OptionsView.swift
@@ -9,11 +9,15 @@ import Combine
 import SwiftUI
 
 struct OptionsView: View {
+	// MARK: - PROPS
 	@Binding var config: ApparatusConfig
 	@Binding var options: ApparatusOptions
 
 	var onChange: ((ApparatusConfig) -> Void)?
+	
+	@State private var showPopover: Bool = true
 
+	// MARK: - BODY
 	var body: some View {
 		VStack {
 			GroupBox(label: Text("shape")) {
@@ -140,6 +144,28 @@ struct OptionsView: View {
 					}
 					//.padding(.horizontal)
 					Text("TODO: add stroke color")
+//					Button("Show popover") {
+//						 self.showPopover = true
+//					}
+					GroupBox {
+						Label {
+							Text("color.rawValue")
+						} icon: {
+							RoundedRectangle(cornerRadius: 4, style: .continuous)
+								.frame(width: 10, height: 10)
+								.foregroundColor(Color("grey.100"))
+						}
+					}.onTapGesture {
+						self.showPopover.toggle()
+					}.popover(
+						isPresented: self.$showPopover,
+						arrowEdge: .bottom
+					) {
+						VStack {
+							
+						}.padding()
+
+					}
 					/*
 					
 					colorMain
@@ -165,7 +191,6 @@ struct OptionsView: View {
 }
 
 // MARK: - PREVIEW
-
 struct OptionsView_Previews: PreviewProvider {
 	static var previews: some View {
 		OptionsView(
@@ -239,7 +264,7 @@ struct IntField: View {
 	}
 }
 
-// MARK: - CONFIG
+// MARK: - MODEL
 
 struct ApparatusConfig: Equatable {
 	var cellCountX: Double
@@ -380,56 +405,4 @@ struct ApparatusOptions: Equatable {
 		self.colorStroke = colorStroke
 		self.colorGround = colorGround
 	}
-}
-
-let bw: [Color] = [
-	Color("grey.100"),
-	Color("grey.200"),
-	Color("grey.300"),
-	Color("grey.400"),
-	Color("grey.500"),
-	Color("grey.600"),
-	Color("grey.700"),
-	Color("grey.800"),
-	Color("grey.900"),
-]
-  
-enum ColorPrimary {
-	case grey, red, orange, yellow, green, blue, purple
-}
-
-enum ColorLuminance: Int {
-	case nearWhite = 100
-	case extraLight = 200
-	case light = 300
-	case normal = 400
-	case medium = 500
-	case semiDark = 600
-	case dark = 700
-	case extraDark = 800
-	case nearBlack = 900
-}
-
-//100 Thin (Hairline)
-//200 Extra Light (Ultra Light)
-//300 Light
-//400 Normal
-//500 Medium
-//600 Semi Bold (Demi Bold)
-//700 Bold
-//800 Extra Bold (Ultra Bold)
-//900 Black (Heavy)
-
-func getColorPalette(
-	primaries: [ColorPrimary],
-	luminance: [ColorLuminance]
-) -> [Color] {
-	var colors = [Color]()
-	for p in primaries {
-		for l in luminance {
-			colors.append(Color("\(p).\(l)"))
-		}
-	}
-
-	return colors
 }
