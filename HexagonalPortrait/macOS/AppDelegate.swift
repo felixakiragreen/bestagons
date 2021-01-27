daedfc6f199f67d667bfb6233574cc4a6b2ee893
Felix Green <work@felix.green>
2021-01-27 13:51:41 -0500

    cleanup

--- HexagonalPortrait/macOS/AppDelegate.swift
+++ HexagonalPortrait/macOS/AppDelegate.swift
@@ -24,43 +24,15 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 			contentRect: NSRect(x: 0, y: 0, width: 480, height: 300),
 			styleMask: [.titled, .closable, .miniaturizable, .resizable, .fullSizeContentView],
 			backing: .buffered, defer: false)
+		
 		window.center()
-//		window.setFrameAutosaveName("Main Window")
+		window.setFrameAutosaveName("Main Window")
 		
-//		window.titlebarAppearsTransparent = true
-//		window.titleVisibility = .hidden
-		
-	
-		
-		
-//		let visualEffect = NSVisualEffectView()
-//		visualEffect.translatesAutoresizingMaskIntoConstraints = false
-//		visualEffect.material = .windowBackground
-//		visualEffect.state = .active
-//		visualEffect.wantsLayer = true
-//		visualEffect.layer?.cornerRadius = 16.0
-		
-//		window.titleVisibility = .hidden
-//		window.styleMask.remove(.titled)
 		window.titlebarAppearsTransparent = true
 		window.styleMask.insert(.fullSizeContentView)
 		
 		window.backgroundColor = NSColor(Color.clear)
 		window.isMovableByWindowBackground = true
-		
-//		window.contentView?.addSubview(visualEffect)
-//		window.contentView = visualEffect
-		
-//		guard let constraints = window.contentView else {
-//			return
-//		}
-		
-//		WindowController
-		
-//		visualEffect.leadingAnchor.constraint(equalTo: constraints.leadingAnchor).isActive = true
-//		visualEffect.trailingAnchor.constraint(equalTo: constraints.trailingAnchor).isActive = true
-//		visualEffect.topAnchor.constraint(equalTo: constraints.topAnchor).isActive = true
-//		visualEffect.bottomAnchor.constraint(equalTo: constraints.bottomAnchor).isActive = true
 		
 		window.contentView = NSHostingView(rootView: contentView)
 		window.makeKeyAndOrderFront(nil)
